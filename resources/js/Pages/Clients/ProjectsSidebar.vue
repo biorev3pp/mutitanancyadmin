f31217a23cd4fb5cd4ecf5e892275baaeab1541d
Ankit kumar <media@biorev.us>
2022-04-12 16:48:57 +0530

    project sidebar css updated

--- resources/js/Pages/Clients/ProjectsSidebar.vue
+++ resources/js/Pages/Clients/ProjectsSidebar.vue
@@ -17,8 +17,8 @@
                     </div>
                     <div class="mb-4">
                         <label for="dropdown-domain" :class="$formClasses.label">Package</label>
-                        <select id="dropdown-domain" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" aria-label="select Package" @change="createSubdomain()" v-model="form.packageInof">
-                            <option value="s">select Package</option>
+                        <select id="dropdown-domain" :class="(form.errors.has('package'))?$formClasses.textInputError:$formClasses.textInput" aria-label="select Package" @change="createSubdomain()" v-model="form.packageInof">
+                            <option value="">Select Package</option>
                             <option v-for="pack in dpackages" :key="pack.id" :value="pack.id+','+pack.domain_name">{{ pack.package_name }}</option>                                
                         </select>
                         <p :class="$formClasses.errorLabel" v-if="form.errors.has('package')" v-html="form.errors.get('package')" />
@@ -26,11 +26,11 @@
                     <div class="mb-4">
                             <label for="subdomain-input" :class="$formClasses.label">Sub Domain</label>
                         <div class="flex">
-                            <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600" @click="submitProject">
+                            <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-sm border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600 cursor-pointer" @click="submitProject">
                                 <BiorevIcon v-show="domainstatus" icon="check" className="flex-shrink-0 w-5 h-5 text-white-500 transition duration-75 dark:text-white-400 group-hover:text-white dark:group-hover:text-white" />
                                 <BiorevIcon v-show="!domainstatus" icon="x" className="flex-shrink-0 w-5 h-5 text-white-500 transition duration-75 dark:text-white-400 group-hover:text-white dark:group-hover:text-white" />
                             </span>
-                            <input type="text" id="subdomain-input" :class="(form.errors.has('name'))?$formClasses.textInputError:$formClasses.textInput" placeholder="Sub Domain" v-model="this.form.subdomain">
+                            <input type="text" id="subdomain-input" :class="(form.errors.has('subdomain'))?$formClasses.textInputError:$formClasses.textInput" placeholder="Sub Domain" v-model="this.form.subdomain">
                         </div>
                             <p :class="$formClasses.errorLabel" v-if="form.errors.has('subdomain')" v-html="form.errors.get('subdomain')" />
                             
@@ -47,8 +47,8 @@
                     </div> -->
                     <div class="mb-4">
                         <label for="dropdown-status" :class="$formClasses.label">Status</label>
-                        <select id="dropdown-status" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" aria-label="select Package" v-model="form.status">
-                            <option value="s">select</option>
+                        <select id="dropdown-status" :class="(form.errors.has('status'))?$formClasses.textInputError:$formClasses.textInput"  v-model="form.status">
+                            <option value="">Select Status</option>
                             <option value="1" selected>Active</option>
                             <option value="0">In-Active</option>
                         </select>
@@ -57,18 +57,18 @@
                     <div class="form-group mb-6">
                         <label for="message" :class="$formClasses.label">Notes</label>
                         <div class="flex">
-                            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a note..." v-model="form.notes"></textarea>
+                            <textarea id="message" rows="4" :class="$formClasses.textInput" placeholder="Leave a note..." v-model="form.notes"></textarea>
                         </div>
                     </div>                    
                 </form>
             
         </div>
         <div class="border-t border-gray-200 h-14 p-3 absolute bottom-0 right-0 w-96 flex justify-between items-center">
-            <button :disabled="form.addNewProject" @click="submitProject" type="button" class="leading-none transition duration-500 inline-flex items-center  focus:outline-none text-white bg-indigo-600 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-sm text-sm px-3 py-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-900">
+            <button :disabled="form.addNewProject" @click="submitProject" type="button"  :class="$formClasses.submitBtn">
                 <svg class="w-5 h-5 mx-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
                 <span>{{ ptitle }}</span>
             </button>
-            <button type="button" class="transition duration-500 inline-flex items-center  focus:outline-none text-red-500 border border-red-500 hover:bg-red-500 hover:text-white focus:ring-4 focus:ring-red-300 font-medium rounded-sm text-sm px-3 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" @click="$emit('changeSidebarStatus')">
+            <button type="button" :class="$buttonClasses.cancelBtn" @click="$emit('changeSidebarStatus')">
                 <svg class="w-4 h-4 mx-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
                 <span>Cancel</span>
             </button>
@@ -93,12 +93,12 @@ export default {
                 client_code : this.clientCode,
                 project_name : '',
                 subdomain : '',
-                packageInof : 's',
+                packageInof : '',
                 package : '',
                 package_id : '',
                 notes : '',
                 addNewProject : false,
-                status : 's',
+                status : '',
                 setup_date : ref(new Date()),
             })
         }
